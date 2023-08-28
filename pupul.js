0b0f787b18650f86aea38ce38777983e405fe17e
Enyerson <135800644+ivanotice@users.noreply.github.com>
2023-08-27 22:11:00 -0400

    Create pupul.js

---
+++ pupul.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+      $('.carousel .slides').slick({
+        slidesToShow: 4, // Mostrar 4 películas a la vez
+        slidesToScroll: 1, // Desplazarse de una película a la vez
+        autoplay: true, // Activar reproducción automática
+        autoplaySpeed: 2000, // Intervalo de tiempo entre cada película (en milisegundos)
+        arrows: false, // Desactivar los botones de siguiente y anterior
+        draggable: true, // Permitir el desplazamiento manual
+        infinite: true // Reiniciar el carrusel automáticamente al llegar al final
+      });
+
+      $('.carousel .slides li img').on('click', function(){
+        // Agregar aquí la animación al hacer clic en una película
+        console.log('Has hecho clic en una película');
+      });
+    });
