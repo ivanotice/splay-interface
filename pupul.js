2d08977898affe2fba9d13075f6a34617bcd2c92
Enyerson <135800644+ivanotice@users.noreply.github.com>
2023-09-03 03:41:47 -0400

    Update pupul.js

--- pupul.js
+++ pupul.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
       $('.carousel .slides').slick({
-        slidesToShow: 4, // Mostrar 4 películas a la vez
+        slidesToShow: 5, // Mostrar 4 películas a la vez
         slidesToScroll: 1, // Desplazarse de una película a la vez
         autoplay: true, // Activar reproducción automática
-        autoplaySpeed: 2000, // Intervalo de tiempo entre cada película (en milisegundos)
+        autoplaySpeed: 1000, // Intervalo de tiempo entre cada película (en milisegundos)
         arrows: false, // Desactivar los botones de siguiente y anterior
         draggable: true, // Permitir el desplazamiento manual
         infinite: true // Reiniciar el carrusel automáticamente al llegar al final
